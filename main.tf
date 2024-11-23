0fb44ab1f96b8882c45bbf035b8ca62db239602a
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:26:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = var.cidr_block
-  availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
+resource "aws_subnet" "subnet_example" {
+  vpc_id     = var.vpc_id
+  cidr_block = var.cidr_block
+}
 
-  tags = {
-    Name = "Subnet"
-  }
+output "subnet_id" {
+  value = aws_subnet.subnet_example.id
 }
