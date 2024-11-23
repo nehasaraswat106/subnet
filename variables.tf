11d1a0d50fa01fa2ca6db954b521ef8d347bf630
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 13:27:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,14 @@
-# Subnet Module Variables
 variable "vpc_id" {
   description = "VPC ID for the subnet"
   type        = string
-  default     = "vpc-0be1f971dc82b5397"
 }
 
 variable "cidr_block" {
   description = "CIDR block for the subnet"
   type        = string
-  default     = "10.0.0.0/24"
 }
 
 variable "availability_zone" {
   description = "Availability zone for the subnet"
   type        = string
-  default     = "us-east-1a"
 }
